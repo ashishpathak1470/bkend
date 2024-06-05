059e4a3db2e755c1780687ca5e17acd8b126678f
Ashish <145586793+ashishpathak1470@users.noreply.github.com>
2024-06-05 16:14:37 +0530

    seacrch

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const userModel = require('./users');
+
 router.get('/', function(req, res, next) {
   res.render('index');
 });
@@ -9,5 +11,43 @@ router.get('/', function(req, res, next) {
 router.get('/failed', function(req, res) {
   req.flash("age", 12);
 });
+
+router.get('/create', async function(req, res){
+  let userdata = await userModel.create({
+    username: "neh",
+    nickname: "sur",
+    discription: "anything im ",
+    categories:['js','node','express', 'react', 'next','mongo']
+  });
+  res.send(userdata);
+});
+
+// case insentitive search
+router.get('/find', async function(req, res){
+  var RegEx = new RegExp("^Surya$", 'i');
+  let user = await userModel.findOne({username: RegEx });
+  res.send(user);
+});
+
+
+// search according to the perticular parameter
+router.get('/read', async function(req, res){
+  let users = await userModel.find({categories: {$all: ['next', 'react', 'mongo']}});
+  res.send(users);
+});
+
+
+// search according to the date
+router.get('/date', async function(req, res){
+  var date1 = new Date('2024-06-04');
+  var date2 = new Date('2024-06-06');
+  let datecreate= await userModel.find({datecreated:{$gte:date1,$lte:date2}});
+  res.send(datecreate);
+});
+
+
+
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+
+mongoose.connect("mongodb://127.0.0.1:27017/testing");
+
+const userSchema= mongoose.Schema({
+  username: String,
+  nickname: String,
+  discription: String,
+  categories:{
+    type: Array,
+    default: []
+  }, 
+  datecreated :{
+    type: Date,
+    default: Date.now()
+  }
+})
+
+const User=mongoose.model("user", userSchema);
+module.exports = User;
